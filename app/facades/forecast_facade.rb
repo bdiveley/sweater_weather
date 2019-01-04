3fc00ed67bc84a157156dbcd51fb39d0b30eaf1b
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-04 10:21:41 -0700

    quick giphy service refactor

--- app/facades/forecast_facade.rb
+++ app/facades/forecast_facade.rb
@@ -12,6 +12,7 @@ class ForecastFacade
     daily_data.map do |daily|
       @giphy_forecast_results << giphy_service(daily[:summary]).get_json[:data][0][:url]
     end
+    binding.pry
     # forecast = Forecast.new(@location, @forecast_results)
     # load_giphy_days(forecast)
   end
--- app/services/giphy_service.rb
+++ app/services/giphy_service.rb
@@ -5,11 +5,17 @@ class GiphyService
   end
 
   def get_json
-    conn = Faraday.new("http://api.giphy.com/")
-    response = conn.get("v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=#{@summary}&limit=1")
     JSON.parse(response.body, symbolize_names: true)
   end
 
 private
+
+  def conn
+    Faraday.new("http://api.giphy.com/")
+  end
+
+  def response
+    conn.get("v1/gifs/search?api_key=#{ENV['GIPHY_API_KEY']}&q=#{@summary}&limit=1")
+  end
 
 end
