377b2819f731854dc712d6a8d48c19ec8f227c64
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-03 19:03:51 -0700

    tep comment out current_user method until its needed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
-  helper_method :current_user
-
-  def current_user
-    @current_lookup_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
+  # helper_method :current_user
+  #
+  # def current_user
+  #   @current_lookup_user ||= User.find(session[:user_id]) if session[:user_id]
+  # end
 end
