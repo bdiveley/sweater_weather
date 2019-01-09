6a42ed05b9ea8a63b4de2c47c6856a1686ecfeb1
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-09 14:35:33 -0700

    Make mailer perform_later function work

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,8 +1,8 @@
 class MailersController < ApplicationController
 
   def create
-    #two options to send mailers - through mailer or background worker 
-    # UserNotifierMailer.inform(params[:email], params[:location]).deliver_now
+    #two options to send mailers - through mailer or background worker
+    # UserNotifierMailer.inform(params[:email], params[:location]).deliver_later
     ForecastSenderJob.perform_later(params[:email], params[:location])
     render body: "Email Sent", status: 200
   end
