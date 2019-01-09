3f160b900c80b81143d9519ac95deb82526bd989
Bailey Diveley <bailey.diveley@gmail.com>
2019-01-09 10:33:16 -0700

    Josh comments from assessment

--- app/controllers/mailers_controller.rb
+++ app/controllers/mailers_controller.rb
@@ -1,8 +1,9 @@
 class MailersController < ApplicationController
 
   def create
-    UserNotifierMailer.inform(params[:email], params[:location]).deliver_now
-    # ForecastSenderJob.perform_later(params[:email], params[:location])
+    #two options to send mailers - through mailer or background worker 
+    # UserNotifierMailer.inform(params[:email], params[:location]).deliver_now
+    ForecastSenderJob.perform_later(params[:email], params[:location])
     render body: "Email Sent", status: 200
   end
 end
--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,9 +1,30 @@
 class Favorite < ApplicationRecord
   belongs_to :user
+  # belongs_to :location
   validates :location, uniqueness: true, presence: true
 
+  # delegates :city, to: :location
+  # delegates :state, to: :location
   def current_weather
     facade = FavoriteFacade.new(location.downcase)
     facade.get_forecast(location)
   end
 end
+
+# In the action_controller
+
+# def user_favorites
+#   current_user.favorites.map do |favorite|
+#     LocationWithWeather.new(favorite)
+#   end
+#
+#   LocationWithWeather.new(location)
+# end
+
+# Decorator
+# class LocationWithWeather < SimpleDelegator
+#   def current_weather
+#     facade = FavoriteFacade.new(location.downcase)
+#     facade.get_forecast("#{city},#{state})
+#   end
+# end
